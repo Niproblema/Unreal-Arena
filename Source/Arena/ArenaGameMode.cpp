8213c2480e9e1a4a9bc815d0c23377b9e871fb47
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2017-08-01 18:51:46 +0200

    Server

--- Source/Arena/ArenaGameMode.cpp
+++ Source/Arena/ArenaGameMode.cpp
@@ -7,7 +7,7 @@
 AArenaGameMode::AArenaGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/Mannequin/Behaviour/BP_TP_Character"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
